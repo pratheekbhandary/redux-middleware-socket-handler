95b1dec2d28a2392ab8c993a8131c7ae5daeded3
pratheekbhandary <pratheekbhandary>
2019-03-24 14:40:35 +0530

    Adding conversation and chatlist reducer

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,3 +1,5 @@
 export const CHAT_SERVER_URL = 'localhost:9001';
 export const SEND_SOCKET ="SEND_SOCKET";
 export const RECEIVE_SOCKET ="RECEIVE_SOCKET";
+export const SEND_MESSAGE ="SEND_MESSAGE";
+export const RECEIVE_MESSAGE ="RECEIVE_MESSAGE";
--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,47 @@
-import { createStore } from 'redux'
-import { createContext } from 'react'
+import { createStore, combineReducers } from "redux";
+import { SEND_MESSAGE, RECEIVE_MESSAGE } from "../CONSTANTS";
 
 export const makeStore = () => {
-  const store = createStore(reducer)
-  return store
-}
+  const store = createStore(
+    combineReducers({
+      conversations: messageReducer,
+      chatList: chatListReducer
+    })
+  );
+  return store;
+};
 
-const reducer = (state = initState, action) => {
-  return state
-}
+const chatListReducer = (state = [], action) => {
+  if( action.type === SEND_MESSAGE) {
+    return filterAndUnshift(state, action.payload.to);
+  } else if (action.type === RECEIVE_MESSAGE) {
+    return filterAndUnshift(state, action.payload.from);
+  }
+  return state;
+};
 
-const initState = {
-  messages: []
+const messageReducer = (state = {}, action) => {
+  if (action.type === SEND_MESSAGE) {
+    return sendMessage(state, payload);
+  } else if (action.type === RECEIVE_MESSAGE) {
+    return receiveMessage(state, payload);
+  }
+  return state;
+};
+
+const receiveMessage = ({ state, payload: message }) => {
+  state[message.from] = [...state[message.from], message];
+  return { ...state };
+};
+
+const sendMessage = ({ state, payload: message }) => {
+  state[message.to] = [...state[message.to], message];
+  return { ...state };
+};
+
+const filterAndUnshift = (array, friend) => {
+  const newarray = array.filter((ele) => {
+    return ele.friendId !== friend.id;
+  });
+  return newarray.unshift(friend);
 }
